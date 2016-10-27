8b64fd3bc299a2c2638d535c36200858930dc7bd
vagrant <vagrant@homestead>
2016-10-27 01:45:27 +0000

    add a form

--- resources/views/messages/write.blade.php
+++ resources/views/messages/write.blade.php
@@ -7,7 +7,33 @@
         We will answer all questions and comments quickly as possible. We appreciate your interest!
     </p>
 
-    <p>
-        Fill out the form below.
-    </p>
+    <div class="row">
+
+        <div class="col-md-4">
+
+            <form class="form" action="/messages/submit" method="post">
+
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" name="name">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Message</label>
+                    <textarea name="message" id="message" class="form-control" rows="5"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-success">Send</button>
+            </form>
+
+        </div>
+
+    </div>
+
+
 @endsection
